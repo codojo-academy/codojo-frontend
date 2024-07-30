d6540f78ec5237f451c351664cc87b24314d67a8
priyankapasrija <104025698+priyankapasrija@users.noreply.github.com>
2024-07-30 21:00:57 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
+import { nextui } from "@nextui-org/react";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    // ...
+    // make sure it's pointing to the ROOT node_module
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+};
