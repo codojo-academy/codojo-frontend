430a082d7d74330adeab335bb01dbecce9900923
priyankapasrija <104025698+priyankapasrija@users.noreply.github.com>
2024-07-31 14:00:00 +0200

    added another component in the home page

--- src/components/SetsUpApart.jsx
+++ src/components/SetsUpApart.jsx
@@ -13,37 +13,35 @@ const SetsUpApart = () => {
       </div>
       <div>
         <GoHome style={{ color: "#E91E63", fontSize: "24px" }} />
-        <h4>Quality of curriculum</h4>
+        <h4>Quality of teachers</h4>
         <p>
-          Personalized curriculum focused on mastery through depth and practice
+          Highly passionate and qualified teachers with relevant background{" "}
         </p>
       </div>
       <div>
         <GoHome style={{ color: "#E91E63", fontSize: "24px" }} />
-        <h4>Quality of curriculum</h4>
+        <h4>Adaptive AI based learning</h4>
+        <p>Learning journey aided by AI ensuring better learning outcomes </p>
+      </div>
+      <div>
+        <GoHome style={{ color: "#E91E63", fontSize: "24px" }} />
+        <h4>Hands-on learning</h4>
         <p>
-          Personalized curriculum focused on mastery through depth and practice
+          Rigorous hands-on practice enabled through 1k+ of exercises &
+          worksheets{" "}
         </p>
       </div>
       <div>
         <GoHome style={{ color: "#E91E63", fontSize: "24px" }} />
-        <h4>Quality of curriculum</h4>
-        <p>
-          Personalized curriculum focused on mastery through depth and practice
-        </p>
+        <h4>Progress monitoring</h4>
+        <p>Highly detailed personalized progress report </p>
       </div>
       <div>
         <GoHome style={{ color: "#E91E63", fontSize: "24px" }} />
-        <h4>Quality of curriculum</h4>
+        <h4>In-class & outside class engagement</h4>
         <p>
-          Personalized curriculum focused on mastery through depth and practice
-        </p>
-      </div>
-      <div>
-        <GoHome style={{ color: "#E91E63", fontSize: "24px" }} />
-        <h4>Quality of curriculum</h4>
-        <p>
-          Personalized curriculum focused on mastery through depth and practice
+          Personalized curriculum focused on mastery through depth andStudent
+          centric fun environment - community events, webinars & more
         </p>
       </div>
     </div>
