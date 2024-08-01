8ecdfd25677e2400c1c44f66a7e0e09f5fe2cb91
priyankapasrija <104025698+priyankapasrija@users.noreply.github.com>
2024-08-01 17:31:23 +0200

    working on home page

--- src/components/CourseInfo.jsx
+++ src/components/CourseInfo.jsx
@@ -1,5 +1,14 @@
 const CourseInfo = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center mb-6 text-gray-800 mt-10">
+        Online courses for kids for all age groups!
+      </h1>
+      <p className="text-center text-gray-600 mb-10 font-medium">
+        Well researched and highly effective curriculum
+      </p>
+    </div>
+  );
 };
 
 export default CourseInfo;
--- src/components/GettingStarted.jsx
+++ src/components/GettingStarted.jsx
@@ -136,12 +136,12 @@ const GettingStarted = () => {
           <CardBody className="overflow-visible py-2"></CardBody>
         </Card>
       </div>
-      <h1 className="text-3xl font-bold text-center mb-6 text-gray-800 mt-10">
+      {/* <h1 className="text-3xl font-bold text-center mb-6 text-gray-800 mt-10">
         Online courses for kids for all age groups!
       </h1>
       <p className="text-center text-gray-600 mb-10 font-medium">
         Well researched and highly effective curriculum
-      </p>
+      </p> */}
     </div>
   );
 };
