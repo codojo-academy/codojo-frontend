d546e963d96754293beeb2572974b6788d6e9d92
priyankapasrija <104025698+priyankapasrija@users.noreply.github.com>
2025-02-19 16:48:49 +0100

    did some changes

--- src/components/demobooking/SecondPage.jsx
+++ src/components/demobooking/SecondPage.jsx
@@ -1,10 +1,11 @@
 import { InlineWidget } from "react-calendly";
 
 const SecondPage = (formData) => {
-    const calendlyUrl = `https://calendly.com/codojo05/demo-session?name=${encodeURIComponent(
-        formData.parent_name)}&email=${encodeURIComponent(formData.parent_email)}`;
+  const calendlyUrl = `https://calendly.com/codojo05/demo-session?name=${encodeURIComponent(
+    formData.parent_name
+  )}&email=${encodeURIComponent(formData.parent_email)}`;
 
-    return <InlineWidget url={calendlyUrl} />;
+  return <InlineWidget url={calendlyUrl} />;
 };
 
 export default SecondPage;
